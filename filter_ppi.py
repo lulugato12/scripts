67adc3d1208fd570b1844630fbbafdc5b2ea81f2
Lulu Cajica <A01732177@itesm.mx>
2020-11-18 15:42:10 -0600

    PPI filter updated

--- filter_ppi.py
+++ filter_ppi.py
@@ -1,13 +1,11 @@
-# Filter PPI with the interested genes only
+# Clean PPI with ids and weight
 # Lourdes B. Cajica
-# 10 - 25 - 20
+# 11 - 18 - 20
 
 import os
 import time
 
 path = "C:/Users/hp/Desktop/redes_data/"
-proteins = list()
-genes = list()
 output_interactions = list()
 count = 0
 
@@ -16,59 +14,24 @@ print("creating new folder...", end="")
 try:
     os.mkdir(path + "output/")                                                  # creates the folder where the data is going to be saved
 except OSError as error:
-    print("the folder already exists.")
+    print("the folder already exists...", end = "")
 
 print("finished.\nreading files...", end = "")
 
-file_proteins = open(path + "output_proteins.txt", "r")
+file_proteins = open(path + "string_interactions.tsv", "r")
 data_proteins = file_proteins.readlines()
 file_proteins.close()
 
-print("finished.\npreparing genes...", end = "")
+print("finished.\ncleaning PPI...")
 
-for p in data_proteins:
-    prep = p.split("\t")
-    proteins.append(prep[1].rsplit("\n")[0])
-    genes.append(prep[0].rsplit("\n")[0])
+for line in data_proteins:
+     l = line.split("\t")
+     p1 = l[2].split(".")[1]
+     p2 = l[3].split(".")[1]
+     w = l[12].split("\n")[0]
+     output_interactions.append(p1 + "\t" + p2 + "\t" + w + "\n")
 
-print("finished.\nfiltering genes...")
-
-for file in os.listdir(path + "ppi/"):
-    file_ppi = open(path + "ppi/" + file, "r")
-    data_ppi = file_ppi.readlines()
-    file_ppi.close()
-    ppi = list()
-
-    print("reading data...", end = "")
-    for p in data_ppi:
-        prep = p.split(" ")
-        g1 = prep[0].split(".")
-        g2 = prep[1].split(".")
-        ppi.append([g1[1].rsplit("\n")[0], g2[1].rsplit("\n")[0]])
-
-    print("finished. filtering...")
-
-    for data in ppi:
-        print("interaction", count, "...", end = "")
-        if data[0] in proteins and data[1] in proteins:
-            output_interactions.append(genes[proteins.index(data[0])] + "\t" + genes[proteins.index(data[1])] + "\t1.0\n")
-            print("got one.", len(output_interactions))
-            if len(output_interactions) == 500:
-                break
-        else:
-            print("not found.", len(output_interactions))
-
-        count += 1
-        
-        if len(output_interactions) == 500:
-            break
-    if len(output_interactions) == 500:
-        break
-    print("finished. sleeping...", end = "")
-    time.sleep(20)
-    print("ready!")
-
-print("filtering finished.\nsaving data...", end = "")
+print("finished.\nsaving data...", end = "")
 
 new_file = open(path + "/output/output_ppi.txt", "w")
 new_file.writelines(output_interactions)
