e83b5868c3d51d4aacc9a745a1541f91898b5745
lulugato12 <A01732177@itesm.mx>
2021-03-15 20:53:05 -0600

    commented the ppi file

--- filter_ppi.py
+++ filter_ppi.py
@@ -5,36 +5,36 @@
 import os
 import time
 
-path = "C:/Users/hp/Desktop/redes_data/"
-output_interactions = list()
+path = "C:/Users/hp/Desktop/redes_data/"                                # path to the current directory
+output_interactions = list()                                            # list that saves the ppi data
 count = 0
 
 print("Creating new folder...", end="")
 
 try:
-    os.mkdir(path + "output/")                                                  # creates the folder where the data is going to be saved
+    os.mkdir(path + "output/")                                          # creates the folder where the data is going to be saved
 except OSError as error:
     print("The folder already exists.", end = " ")
 
 print("finished.\nReading files...", end = "")
 
-file_proteins = open(path + "string_interactions.tsv", "r")
-data_proteins = file_proteins.readlines()
+file_proteins = open(path + "string_interactions.tsv", "r")             # opens the file that contiains the protein interactions
+data_proteins = file_proteins.readlines()                               # reads the data
 file_proteins.close()
 
 print("finished.\nCleaning PPI...")
 
 for line in data_proteins:
-     l = line.split("\t")
-     p1 = l[2].split(".")[1]
-     p2 = l[3].split(".")[1]
-     w = l[12].split("\n")[0]
-     output_interactions.append(p1 + "\t" + p2 + "\t" + w + "\n")
+     l = line.split("\t")                                               # splits the data line
+     p1 = l[2].split(".")[1]                                            # saves the first protein
+     p2 = l[3].split(".")[1]                                            # saves the second protein
+     w = l[12].split("\n")[0]                                           # saves the interaction weight
+     output_interactions.append(p1 + "\t" + p2 + "\t" + w + "\n")       # sabes the data
 
 print("finished.\nSaving data...", end = "")
 
-new_file = open(path + "/output/ppi.txt", "w")
-new_file.writelines(output_interactions)
+new_file = open(path + "/output/ppi.txt", "w")                          # creates a new file to save the data
+new_file.writelines(output_interactions)                                # saves the data
 new_file.close()
 
 print("finished.\nData saved in", path + "output/ppi.txt.")
