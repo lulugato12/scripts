c6d13feb79cd536c8624d056f4ac733224f78694
lulugato12 <A01732177@itesm.mx>
2021-03-18 15:43:20 -0600

    fixed path, fixed datos folder

--- filter_motifs.py
+++ filter_motifs.py
@@ -5,12 +5,13 @@
 import os
 import time
 
-path = "C:/Users/hp/Desktop/redes_data/"                                # path to the current directory
+path = "/datos/ot/lbcajica/"                                            # path to the current directory
 tf_name = list()                                                        # list of motif names
 gene_name = list()                                                      # list of gene names
 tf_id = list()                                                          # list of motif ids
 gene_id = list()                                                        # list of gene ids
 output = list()                                                         # output data lines
+count = 0
 
 print("Creating new folder...", end=" ")
 
@@ -21,11 +22,11 @@ except OSError as error:
 
 print("finished.\nReading files...", end = " ")
 
-file_motif = open(path + "ToyMotifData.txt", "r")                       # opens the file that contains the motif data
+file_motif = open(path + "datos/ToyMotifData.txt", "r")                 # opens the file that contains the motif data
 original = file_motif.readlines()                                       # reads all the data
 file_motif.close()
 
-file_names = open(path + "mart_export.txt", "r")                        # opens the file that contians the name-id relation
+file_names = open(path + "datos/mart_export.txt", "r")                  # opens the file that contians the name-id relation
 update = file_names.readlines()                                         # reads all the data
 file_names.close()
 
@@ -39,15 +40,16 @@ for l in update:
     tf_name.append(line[3].split("\n")[0])                              # saves the motif name
 
 print("finished.\nFiltering names...")
-i = 0
+
 for p in original:
-    print("line", i, "...", end = " ")
+    print("line " + count + "...", end = " ")
     prep = p.split("\t")                                                # splits the data line to get the motif and the gene
     line = ""
     if prep[0] in gene_name and prep[1] in gene_name:                   # check if both the motif and the gene exists in the registries
+        print("got one.", end = " ")
         line += tf_id[gene_name.index(prep[0])] + "\t" + gene_id[gene_name.index(prep[1])] + "\t1.0\n"
         output.append(line)                                             # saves the motif id, the gene id and the weight 1.0
-    i += 1
+    count += 1
     print("finished.")
 
 print("finished.\nSaving data...", end = " ")
@@ -56,4 +58,4 @@ new_file = open(path + "output/motif.txt", "w")                         # create
 new_file.writelines(output)                                             # saves the data
 new_file.close()
 
-print("finished.\nData saved in", path + "motif.txt.")
+print("finished.\nData saved in " + path + "motif.txt.")
