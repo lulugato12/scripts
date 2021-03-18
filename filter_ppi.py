57307a64617544a4fc94701f23a8fd4b0bed0218
lulugato12 <A01732177@itesm.mx>
2021-03-18 15:40:07 -0600

    fixed identation, fixed protein.links folred, fixed comments

--- filter_ppi.py
+++ filter_ppi.py
@@ -5,7 +5,7 @@
 import os
 import time
 
-path = "C:/Users/hp/Desktop/redes_data/"                                # path to the current directory
+path = "/datos/ot/lbcajica/"                                            # path to the current directory
 output_interactions = list()                                            # list that stores the ppi data
 count = 0
 
@@ -18,26 +18,31 @@ except OSError as error:
 
 print("finished.\nReading files...", end = "")
 
-file_ppi = open(path + "9606.protein.links.v11.0.txt", "r")             # opens the file that contiains the protein interactions
+file_ppi = open(path + "datos/9606.protein.links.v11.0.txt", "r")       # opens the file that contiains the protein interactions
 data_ppi = file_ppi.readlines()                                         # reads the data
 file_ppi.close()
 
 data_ppi = data_ppi[1:]                                                 # deletes the head row
 
-file_proteins = open(path + "output/genes.txt", "r")                    # opens the file that contains the proteins
+file_proteins = open(path + "output/proteins.txt", "r")                 # opens the file that contains the proteins
 data_proteins = file_proteins.readlines()                               # reads the data
 file_proteins.close()
 
 print("finished.\nFiltering PPI...")
 
 for line in data_ppi:
-     l = line.split(" ")                                               # splits the data line
-     p1 = l[0].split(".")[1]                                            # reads the first protein
-     p2 = l[1].split(".")[1]                                            # reads the second protein
-     w = "0." + l[2].split("\n")[0]                                     # reads the interaction weight
+    print("Interaction " + count + "...", end = " ")
+    l = line.split(" ")                                                # splits the data line
+    p1 = l[0].split(".")[1]                                            # reads the first protein
+    p2 = l[1].split(".")[1]                                            # reads the second protein
+    w = "0." + l[2].split("\n")[0]                                     # reads the interaction weight
 
-     if l[0] + "\n" in data_proteins and l[1] + "\n" in data_proteins:
+    if l[0] + "\n" in data_proteins and l[1] + "\n" in data_proteins:
+         print("got one.", end = " ")
          output_interactions.append(p1 + "\t" + p2 + "\t" + w + "\n")   # saves the data
+
+    print("finsihed.")
+    count += 1
 
 print("finished.\nSaving data...", end = "")
 
