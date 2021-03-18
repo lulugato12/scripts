3d5dadacc367503210b6ef8552dad04e66731f9c
lulugato12 <A01732177@itesm.mx>
2021-03-18 15:32:27 -0600

    fix head line, fix split "\t" -> "\n"

--- filter_ppi.py
+++ filter_ppi.py
@@ -22,6 +22,8 @@ file_ppi = open(path + "9606.protein.links.v11.0.txt", "r")             # opens 
 data_ppi = file_ppi.readlines()                                         # reads the data
 file_ppi.close()
 
+data_ppi = data_ppi[1:]                                                 # deletes the head row
+
 file_proteins = open(path + "output/genes.txt", "r")                    # opens the file that contains the proteins
 data_proteins = file_proteins.readlines()                               # reads the data
 file_proteins.close()
@@ -29,10 +31,10 @@ file_proteins.close()
 print("finished.\nFiltering PPI...")
 
 for line in data_ppi:
-     l = line.split("\t")                                               # splits the data line
+     l = line.split(" ")                                               # splits the data line
      p1 = l[0].split(".")[1]                                            # reads the first protein
      p2 = l[1].split(".")[1]                                            # reads the second protein
-     w = "0." + l[3].split("\n")[0]                                     # reads the interaction weight
+     w = "0." + l[2].split("\n")[0]                                     # reads the interaction weight
 
      if l[0] + "\n" in data_proteins and l[1] + "\n" in data_proteins:
          output_interactions.append(p1 + "\t" + p2 + "\t" + w + "\n")   # saves the data
