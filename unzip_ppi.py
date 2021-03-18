833c42b0b80354265f59e52466e192598833424e
lulugato12 <A01732177@itesm.mx>
2021-03-18 15:23:44 -0600

    added print statements

--- unzip_ppi.py
+++ unzip_ppi.py
@@ -6,9 +6,14 @@ import os
 import gzip
 
 path = "/datos/ot/lbcajica/"                                                # base path
+
+print("Starting unziping ppi data...", end = " ")
+
 try:
     txt = open(path + "datos/protein.links.v11.0.txt", 'wb')                # new .txt files
     gzfile = gzip.open(path + "datos/protein.links.v11.0.txt.gz", 'rb')     # zipped folder
     txt.writelines(gzfile)                                                  # final file
 except gzip.OSError as e:                                                   # if something goes wrong
     print("Not a valid .gz file")
+
+print("finished.\n Data saved in " + path + "datos/protein.links.v11.0.txt")
