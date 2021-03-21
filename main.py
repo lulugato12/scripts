13df8bbdba8669d0f6215a1cb6c0d0a5a651110a
lulugato12 <A01732177@itesm.mx>
2021-03-20 18:49:29 -0600

    comment the code, add memory limit, time measure function, log file

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
-print("importando librerias...", end = "")
-import os
+# requirements for resource monitoring
+import time
+import resource
+
+# NetZooPy requirements
 from netZooPy.panda import Panda
 from netZooPy.lioness import Lioness
 from netZooPy.lioness.analyze_lioness import AnalyzeLioness
 import pandas as pd
 
-path = "/datos/ot/lbcajica/output"
-genes = path + "genes.txt"
-ppi = path + "ppi.txt"
-motif = path + "motif.txt"
+# function to execute the pipeline of the LIONESS algorithm
+def lioness_exec(panda_obj, nodes):
+    lioness_obj = Lioness(panda_obj)                                                # LIONESS object generator
+    lioness_obj.save_lioness_results(file = 'lioness.txt')                          # store the LIONESS data
 
-print("finished. running pandas...")
+    analyze_lioness_obj = AnalyzeLioness(lioness_obj)                               # network generator
+    analyze_lioness_obj.top_network_plot(top = nodes, file = "lioness_top_40.png")  # plot generator up to X nodes
 
-panda_obj = Panda(genes, motif, ppi, remove_missing=False, keep_expression_matrix=True, save_memory=False)
+# function to set a limit of RAM resource
+def limit_memory(maxsize):
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (maxsize, hard))
 
-print("finished.\nrunning lioness...")
+# variables definition
+path = "/datos/ot/lbcajica/output/"                                                 # base path to the data folder
+log = open(path + "log.txt", "a+")                                                  # log file
+genes = path + "genes.txt"                                                          # genes file
+ppi = path + "ppi.txt"                                                              # ppi file
+motif = path + "motif.txt"                                                          # motif file
+nodes = 40                                                                          # amount of nodes for the final plot
+RAM_max = 40                                                                        # RAM max value in Gigabytes
 
-lioness_obj = Lioness(panda_obj)
-lioness_obj.save_lioness_results(file = 'lioness.txt')
+try:
+    limit_memory(RAM_max * 1024 * 1024 * 1024)                                      # RAM max_limit | 40 G
 
-analyze_lioness_obj = AnalyzeLioness(lioness_obj)
-analyze_lioness_obj.top_network_plot(top = 40, file = "lioness_top_40.png")
+    print("Running pandas...")
+    @profile(presicion = 3, stream = log)
+    panda_obj = Panda(genes, motif, ppi, remove_missing=False, keep_expression_matrix=True, save_memory=False)
 
-print("finished.")
+    print("finished.\nRunning lioness...")
+    @profile(presicion = 3, stream = log)
+    lioness_exec(panda_obj, nodes)
+
+    print("finished. It took", time.thread_time()/60, "min")                        # time execution
+except MemoryError:                                                                 # rised error due to RAM limit
+    print("Memory exceeded :(")
+    log.write("Memory exceeded :(.\n")
