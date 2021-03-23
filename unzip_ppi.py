80d1df1068132a491ec0ddda16f3f8e90b4691bf
lulugato12 <A01732177@itesm.mx>
2021-03-23 09:45:25 -0600

    change ppi .gz file

--- unzip_ppi.py
+++ unzip_ppi.py
@@ -10,8 +10,8 @@ path = "/datos/ot/lbcajica/"                                                # ba
 print("Starting unziping ppi data...", end = " ")
 
 try:
-    txt = open(path + "datos/protein.links.v11.0.txt", 'wb')                # new .txt files
-    gzfile = gzip.open(path + "datos/protein.links.v11.0.txt.gz", 'rb')     # zipped folder
+    txt = open(path + "datos/9606.protein.links.v11.0.txt", 'wb')                # new .txt files
+    gzfile = gzip.open(path + "datos/9606.protein.links.v11.0.txt.gz", 'rb')     # zipped folder
     txt.writelines(gzfile)                                                  # final file
 except OSError as e:                                                        # if something goes wrong
     print("Not a valid .gz file")
