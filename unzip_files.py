0f5785257ffeeb71261982a7ba03678ec30a3c7a
Lourdes Cajica <A01732177@itesm.mx>
2021-10-19 18:30:00 -0500

    add Timer class + clean the style

--- unzip_files.py
+++ unzip_files.py
@@ -1,55 +1,37 @@
 # Unzip gene cases data from GDC
 # Lourdes B. Cajica
-# 15 - 3 - 21
+# Last update: 19 - 10 - 21
 
 import os
 import gzip
 import xtarfile as tarfile
+from timer import Timer
 
-path = "/datos/ot/lbcajica/"                                             # path that contains the actual folder
-folder = path + "datos/breast_data.gz"                                 # initial .gz file
-count = 0
-bad = list()                                                            # this list saves the non .gz file names
+path = # insert path
+read_file = # insert .gz file | ej. "gdc_download_20211006_173713.629179.tar.gz"
 
-print("Creating folders...", end = " ")
-
-try:
-    os.mkdir(path + "file/")                                            # creates the folder for the unzipped folders
-    os.mkdir(path + "data/")                                            # creates the folder where the data is going to be saved
-except OSError as error:
-    print("the folders already exists.")
-
-print("finished.\nUnziping folder...", end = " ")
-
-f = tarfile.open(folder, 'r:gz')                                        # unziping the .gz file
-f.extractall(path = path + "file/")                                     # saving the data in the file/ folder
-
-print("finished.\nStarting extraction.")
-
-for dir in os.listdir(path + "file/"):                                  # navigates through the folder of cases
-    if not dir.endswith(".txt"):                                        # ignores the txt files
-        print("file " + str(count) + "...", end = " ")
-        for gz in os.listdir(path + "file/" + dir + "/"):               # navigates through the case files
-            if not gz.endswith(".txt"):                                 # ignores the txt files
-                try:
-                    txt = open(path + "data/" + dir + ".txt", 'wb')             # new .txt files
-                    gzfile = gzip.open(path + "file/" + dir + "/" + gz, 'rb')   # zipped folder
-                    txt.writelines(gzfile)
-                except OSError as e:                                            # if something goes wrong
-                    print("Not a valid .gz file")
-                    bad.append(gz)
-        count += 1
-        print("finished.")
-
-if len(bad) != 0:                                                       # if any bad file, saves it
+with Timer("Creting folders..."):
     try:
-        os.mkdir(path + "badfiles/")                                    # creates the folder with the bad data
+        os.mkdir(path + "file/")            # creates the folder for the unzipped folders
+        os.mkdir(path + "data/")            # creates the folder where the data is going to be saved
     except OSError as error:
-        print("the folder already exists.")
+        print("the folders already exists.")
 
-    print("Saving bad files...")
-    bfiles = open(path + "badfiles/output.txt", "w")                    # creates a new file
-    bfiles.writelines(bad)                                              # stores the data
-    bfiles.close()
+with Timer("Unziping folder..."):
+    f = tarfile.open(read_file, 'r:gz')     # unziping the .gz file
+    f.extractall(path = path + "file/")     # saving the data in the file/ folder
 
-print("finished.\nSaved data in " + path + "data/")
+with Timer("Starting extraction..."):
+    for dir in os.listdir(path + "file/"):              # navigates through the folder of cases
+        if not dir.endswith(".txt"):                    # ignores the txt files
+            print("Extracting :" + dir + "...")
+            for gz in os.listdir(path + "file/" + dir + "/"):           # navigates through the case files
+                if not gz.endswith(".txt"):                             # ignores the txt files
+                    try:
+                        txt = open(path + "data/" + dir + ".txt", 'wb')             # new .txt files
+                        gzfile = gzip.open(path + "file/" + dir + "/" + gz, 'rb')   # zipped folder
+                        txt.writelines(gzfile)
+                    except OSError as e:
+                        print("Not a valid .gz file")
+
+print('Data saved in' + path + 'data/')
